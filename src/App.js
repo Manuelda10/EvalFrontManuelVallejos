df6b873aa48d4baf871ed253b4b7ffa50d32676e
Manuel Vallejos <manuel.vallejos@unmsm.edu.pe>
2022-06-30 17:33:53 -0500

    Add Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home></Home>
+      <Login></Login>
     </div>
   );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import logoImg from '../../assets/logo.png'
+import './index.css'
+
+const Login = () => {
+    return (<div className='login'>
+        <div className='login-form-container'>
+            <div className='login-container-inside'>
+                <h1>Bienvenido a BPI</h1>
+                <h3>Inicia Sesión</h3>
+                <div className='login-form'>
+                    <form>
+                        <input type='text' placeholder='username'></input><br></br>
+                        <input type='password' placeholder='password'></input><br></br>
+                        <input type='checkbox'></input>
+                        <label>Lorem impusm for sd afasdfsdf aw ejpwas</label>
+                        <br></br>
+                        <button>Iniciar sesión</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div className='login-img-container'>
+            <img src={logoImg} alt='Usuario'></img>
+        </div>
+    </div>
+    )
+}
+
+export default Login
